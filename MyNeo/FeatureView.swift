bacfc4febe14d11133075208982b19886928ca2a
苏强 <>
2025-12-24 10:15:37 +0800

    fix：修改

--- MyNeo/FeatureView.swift
+++ MyNeo/FeatureView.swift
@@ -8,8 +8,6 @@ struct FeatureView: View {
                 Text("功能页面")
                     .font(.title)
                 // 功能模块内容...
-                Text("功能页面")
-                    .font(.title)
             }
             .navigationTitle("功能")
         }
